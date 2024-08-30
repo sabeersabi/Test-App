c31f4bd09378ea0980ce39b557d5f51a299b3490
sabeersabi <112475489+sabeersabi@users.noreply.github.com>
2024-08-30 21:51:43 +0530

    Update App.xaml.cs

--- Test App/App.xaml.cs
+++ Test App/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
@@ -45,10 +45,24 @@ namespace Test_App
         {
             try
             {
+                //m_window = new MainWindow();
+                //m_window.Activate();
                 m_window = new MainWindow();
-                m_window.Activate();
-                var view = ApplicationView.GetForCurrentView();
-                view.TryResizeView(new Size(500, 870));
+                var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(m_window);
+                Microsoft.UI.WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+                Microsoft.UI.Windowing.AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+                if (appWindow is not null)
+                {
+                    Microsoft.UI.Windowing.DisplayArea displayArea = Microsoft.UI.Windowing.DisplayArea.GetFromWindowId(windowId, Microsoft.UI.Windowing.DisplayAreaFallback.Nearest);
+                    if (displayArea is not null)
+                    {
+                        var CenteredPosition = appWindow.Position;
+                        CenteredPosition.X = ((displayArea.WorkArea.Width - appWindow.Size.Width) / 2);
+                        CenteredPosition.Y = ((displayArea.WorkArea.Height - appWindow.Size.Height) / 2);
+                        appWindow.Move(CenteredPosition);
+                    }
+                }
+                m_window.Activate();                
             }
             catch (Exception ex)
             {
